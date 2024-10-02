88917016d86e9c0545f6e6dc4930f50e5341aefa
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-10-02 17:40:37 +0530

    fetch current acc

--- server/Controllers/SecureControllers.js
+++ server/Controllers/SecureControllers.js
@@ -1,5 +1,17 @@
 const User = require("../Models/UserModel");
 
+const profile = async(req,res) =>{
+    try{
+        const {userId} = req.user;
+        const response = await User.findOne({_id:userId});
+        res.status(200).json({message:"user fetched successfully", response});
+    }
+    catch(error){
+        console.error("Fetching error",error)
+        return res.status(400).json({error:"error while fetching profile"})
+    }
+};
+
 const updateUser = async(req,res) => {
     try{
         const {userId} = req.user;
@@ -24,8 +36,8 @@ const updateUser = async(req,res) => {
         res.status(200).json({message:"User updated successfully", response }); 
     }
     catch(error){
-        console.error('Registration error:', error);
+        console.error('Updation error:', error);
         res.status(500).json({ error: 'Server error while updated profile.' });
     };
 };
-module.exports = {updateUser};
+module.exports = {profile, updateUser};
--- server/Endpoints/SecureRoutes.js
+++ server/Endpoints/SecureRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const {updateUser} = require('./../Controllers/SecureControllers');
+const {profile,updateUser} = require('./../Controllers/SecureControllers');
 const ValidationMiddleware = require('../Middlewares/ValidationMiddleware');
 
-router.put("/updateUser",ValidationMiddleware("updateUser"),updateUser);
+router.get("/users/profile",profile);
+router.put("/Users/update",ValidationMiddleware("updateUser"),updateUser);
 
 module.exports = router;
