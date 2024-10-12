b84c7baa1dc111de2dee0428af6c34b4d9c51cc8
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-10-12 10:52:11 +0530

    Commit

--- Client/src/components/FormField/FormField.jsx
+++ Client/src/components/FormField/FormField.jsx
@@ -1,23 +1,41 @@
-function FormField({ name, type, placeholder, value, onChange, label }) {
+import React,{Fragment} from 'react';
+function FormField({ name, type, placeholder, value, onChange, label, className }) {
     return (
         <>
-            <input id={name} value={value} onChange={onChange} name={name} type={type} placeholder={placeholder} />
-            {label ? <label id={name} htmlFor={name}>{label}</label> : null}
+            <input 
+                id={name} 
+                className={className} 
+                value={value} 
+                onChange={onChange} 
+                name={name} 
+                type={type} 
+                placeholder={placeholder} 
+            />
+            {label ? <label htmlFor={name}>{label}</label> : null}
         </>
-    )
+    );
 }
+
 export default function Form({ formFields, onSubmit, error, errorMessages }) {
-
-    return <form onSubmit={onSubmit}>
-        {
-            formFields.map((field, index) => (
-                <>
-                    <FormField value={field.value} onChange={field.onChange} name={field.name} type={field.type} label={field?.label} placeholder={field?.placeholder} key={index} />
-                    {error[field.name] ? <p>{errorMessages[field.name].message}</p> : null}
-                </>
-            ))
-        }
-        <button type="submit">Submit</button>
-    </form >
-
+    return (
+        <form onSubmit={onSubmit}>
+            {formFields.map((field, index) => (
+                <Fragment key={index}>
+                    <FormField 
+                        value={field.value} 
+                        onChange={field.onChange} 
+                        name={field.name} 
+                        type={field.type} 
+                        label={field?.label} 
+                        placeholder={field?.placeholder} 
+                        className={field.className} 
+                    />
+                    {error[field.name] && (
+                        <p>{errorMessages[field.name]?.message}</p>
+                    )}
+                </Fragment>
+            ))}
+            <button type="submit">Submit</button>
+        </form>
+    );
 }
--- Client/src/pages/Register User/RegisterUser.jsx
+++ Client/src/pages/Register User/RegisterUser.jsx
@@ -1,8 +1,8 @@
 import Form from "./../../components/FormField/FormField";
-
 import { useState } from "react";
-// import { register } from "../../services/auth";
 import { useNavigate } from "react-router-dom";
+import styles from './RegisterUser.module.css';
+
 export default function Register() {
     const navigate = useNavigate();
     const [formData, setFormData] = useState({
@@ -21,124 +21,130 @@ export default function Register() {
         confirmPassword: false,
         checkBox: false
     });
+
     const formFields = [
         {
             name: "name",
             type: "text",
             placeholder: "Enter your name",
             value: formData.name,
-            onChange: (e) => {
-                setFormData({ ...formData, name: e.target.value })
-            }
+            onChange: (e) => setFormData({ ...formData, name: e.target.value }),
+            className: styles.formFields
         },
         {
             name: "email",
             type: "email",
             placeholder: "Enter your email",
             value: formData.email,
-            onChange: (e) => {
-                setFormData({ ...formData, email: e.target.value })
-            }
+            onChange: (e) => setFormData({ ...formData, email: e.target.value }),
+            className: styles.formFields
         },
         {
             name: "password",
             type: "password",
             placeholder: "Enter your password",
             value: formData.password,
-            onChange: (e) => {
-                setFormData({ ...formData, password: e.target.value })
-            }
-        }, {
+            onChange: (e) => setFormData({ ...formData, password: e.target.value }),
+            className: styles.formFields
+        },
+        {
             name: "confirmPassword",
             type: "password",
             placeholder: "Confirm your password",
             value: formData.confirmPassword,
-            onChange: (e) => {
-                setFormData({ ...formData, confirmPassword: e.target.value })
-            }
-        }, {
+            onChange: (e) => setFormData({ ...formData, confirmPassword: e.target.value }),
+            className: styles.formFields
+        },
+        {
             name: "mobile",
             type: "text",
             placeholder: "Enter your mobile number",
             value: formData.mobile,
-            onChange: (e) => {
-                setFormData({ ...formData, mobile: e.target.value })
-            }
-        }, {
+            onChange: (e) => setFormData({ ...formData, mobile: e.target.value }),
+            className: styles.formFields
+        },
+        {
             name: "checkBox",
             type: "checkbox",
             label: "Agree to terms and conditions",
-            value: formData.checkBox,
-            onChange: (e) => {
-                setFormData({ ...formData, checkBox: e.target.checked })
-            }
+            checked: formData.checkBox,
+            onChange: (e) => setFormData({ ...formData, checkBox: e.target.checked }),
+            className: styles.textbox
         }
-    ]
+    ];
+
     const errorMessages = {
         name: {
             message: "Name is required",
             isValid: formData.name.length > 0,
-            onError: () => {
-                setError((error) => ({ ...error, name: true }))
-            }
+            onError: () => setError((prev) => ({ ...prev, name: true }))
         },
         email: {
             message: "Email is required",
             isValid: formData.email.length > 0,
-            onError: () => {
-                setError((error) => ({ ...error, email: true }))
-            }
+            onError: () => setError((prev) => ({ ...prev, email: true }))
         },
         password: {
             message: "Password is required",
             isValid: formData.password.length > 0,
-            onError: () => {
-                setError((error) => ({ ...error, password: true }))
-            }
+            onError: () => setError((prev) => ({ ...prev, password: true }))
         },
         confirmPassword: {
             message: "Passwords do not match",
             isValid: formData.confirmPassword === formData.password,
-            onError: () => {
-                setError((error) => ({ ...error, confirmPassword: true }))
-            }
+            onError: () => setError((prev) => ({ ...prev, confirmPassword: true }))
         },
         mobile: {
             message: "Mobile number is required",
-            isValid: formData.mobile.length > 0,
-            onError: () => {
-                setError((error) => ({ ...error, mobile: true }))
-            }
+            isValid: formData.mobile.length == 10,
+            onError: () => setError((prev) => ({ ...prev, mobile: true }))
         },
         checkBox: {
             message: "You must agree to terms and conditions",
             isValid: formData.checkBox,
-            onError: () => {
-                setError((error) => ({ ...error, checkBox: true }))
-            }
+            onError: () => setError((prev) => ({ ...prev, checkBox: true }))
         }
-    }
+    };
+
     const onSubmit = async (e) => {
-        let isError = false;
         e.preventDefault();
-        Object.keys(errorMessages).forEach(key => {
+        let isError = false;
+
+        const updatedError = {};
+
+        Object.keys(errorMessages).forEach((key) => {
             if (!errorMessages[key].isValid) {
                 isError = true;
+                updatedError[key] = true;
                 errorMessages[key].onError();
+            } else {
+                updatedError[key] = false;
             }
-        })
-        // if (!isError) {
-        //     const res = await register(formData);
-        //     if (res.status === 201) {
-        //         alert("Registered successfully");
-        //         navigate("/login"); 
-        //     }
-        //     else {
-        //         alert("Something went wrong");
-        //     }
-        // }
-    }
+        });
+
+        setError(updatedError);
+
+        if (!isError) {
+            // Uncomment when the register function is implemented
+            // const res = await register(formData);
+            // if (res.status === 201) {
+            //     alert("Registered successfully");
+            //     navigate("/login"); 
+            // } else {
+            //     alert("Something went wrong");
+            // }
+        }
+    };
+
     return (
-        <><p>Register</p><Form error={error} formFields={formFields} onSubmit={onSubmit} errorMessages={errorMessages} /></>
-    )
+        <>
+            <p>Register</p>
+            <Form 
+                error={error} 
+                formFields={formFields} 
+                onSubmit={onSubmit} 
+                errorMessages={errorMessages} 
+            />
+        </>
+    );
 }
